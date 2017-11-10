bea5041db2d24595e63d7bcba74283b943ac3886
Gerardo <gerardo_dibli@hotmail.com>
2017-11-10 09:52:07 -0300

    correccion abm attributo

--- AgricolaBackEnd/src/main/java/com/softdelsur/agricola/controller/VariedadController.java
+++ AgricolaBackEnd/src/main/java/com/softdelsur/agricola/controller/VariedadController.java
@@ -2,12 +2,16 @@ package com.softdelsur.agricola.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +46,14 @@ public class VariedadController {
 	public void eliminarVariedad(@PathVariable("idVariedad") String idVariedad) {
 		variedadService.eliminarVariedad(idVariedad);
 		
+	}
+	
+	@CrossOrigin
+	@PostMapping("/save")
+	public void guardarVariedad(@RequestBody VariedadModel variedadModel) {
+		
+		System.out.println(variedadModel.getNombre()+" este es el nombre");
+		variedadService.addVariedad(variedadConverter.convertVariedadModelToVariedad(variedadModel));
 	}
 
 	
--- AgricolaBackEnd/src/main/java/com/softdelsur/agricola/converter/VariedadConverter.java
+++ AgricolaBackEnd/src/main/java/com/softdelsur/agricola/converter/VariedadConverter.java
@@ -3,13 +3,20 @@ package com.softdelsur.agricola.converter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.softdelsur.agricola.entity.Variedad;
 import com.softdelsur.agricola.model.VariedadModel;
+import com.softdelsur.agricola.service.VariedadService;
 
 @Component("variedadConverter")
 public class VariedadConverter {
+	
+	@Autowired
+	@Qualifier("variedadServiceImpl")
+	VariedadService variedadService;
 	
 	public VariedadModel convertVariedadToVariedadModel(Variedad variedad){
 		
@@ -33,4 +40,22 @@ public class VariedadConverter {
 		return variedadModelList;
 
 }
+	
+	public Variedad convertVariedadModelToVariedad(VariedadModel variedadModel) {
+		Variedad variedad = null;
+		
+		if (variedadModel.getId() == null) {
+			variedad = new Variedad();
+			variedad.setEstado(true);
+			variedad = variedadService.addVariedad(variedad);
+		}else {
+			variedad = variedadService.findVariedadById(variedadModel.getId());
+		}
+		variedad.setNombre(variedadModel.getNombre());
+		variedad.setColorVariedad(variedadModel.getColorVariedad());
+		
+		return variedad;
+	}
+	
+	
 }
